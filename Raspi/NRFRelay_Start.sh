cc5c82a9e9d0bdeff1b3e4f37efa0b6497b59485
CHM <110084554+neanderthalbrainman@users.noreply.github.com>
2025-08-08 13:17:52 -0400

    Update NRFRelay_Start.sh

--- Raspi/NRFRelay_Start.sh
+++ Raspi/NRFRelay_Start.sh
@@ -7,4 +7,4 @@ echo "docker exec -it mysql-test1 mysql -u user -p"
 
 cd ~/Documents/RF24/pyRF24
 source venv/bin/activate
-python3 ~/Documents/NRF_py/nrf_5.py
+python3 ~/Documents/NRF_py/ingest_relays.py
