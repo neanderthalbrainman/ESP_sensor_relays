4589e7ee8d72657bbcd210b4ad2ec78b9b7a88dc
CHM <110084554+neanderthalbrainman@users.noreply.github.com>
2025-08-08 13:17:28 -0400

    Create NRFRelay_Start.sh

---
+++ Raspi/NRFRelay_Start.sh
@@ -0,0 +1,10 @@
+cd ~/dockers/mysql
+docker compose up -d
+
+
+echo "To login to mysql:"
+echo "docker exec -it mysql-test1 mysql -u user -p"
+
+cd ~/Documents/RF24/pyRF24
+source venv/bin/activate
+python3 ~/Documents/NRF_py/nrf_5.py
